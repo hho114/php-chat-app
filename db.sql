c471ff3682c64c0f1e15ecadb27d321326d5c5f6
hho114 <hho114@csu.fullerton.edu>
2020-04-19 23:45:54 -0700

    update finish assignment 3

--- db.sql
+++ db.sql
@@ -2,18 +2,21 @@
 
 CREATE TABLE user
 (
+    uid INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25) NOT NULL
+
+);
+
+CREATE TABLE chatlog
+(
     id INT(11)
     AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR
-    (25) NOT NULL
-    );
-
-    CREATE TABLE chatlog
-    (
-        id INT(11)
-        AUTO_INCREMENT PRIMARY KEY,message TEXT,sent_by VARCHAR
-        (50),date_created INT
-        (11));
+    message TEXT,
+    sent_by VARCHAR(50),
+    usr_id INT(11) NOT NULL,
+    usr_name VARCHAR(25) NOT NULL,
+    date_created INT(11)
+);
 
     
 
--- index.php
+++ index.php
@@ -1,31 +1,38 @@
-
 <?php
 
 session_start();
 
+//user already login go to chat page
 if (isset($_SESSION['name'])) {
-	header("Location: /home.php");
+	header("Location: /chat.html");
 }
 
-require 'database.php';
+// require 'database.php';
 
+$db = mysqli_connect('localhost', 'root', 'root', 'chat_db');
 $message = '';
 
 if (!empty($_POST['name'])) :
-	
-	
 
 	// Enter the new user in the database
-	$sql = "INSERT INTO user (name) VALUES (:name)";
-	$stmt = $conn->prepare($sql);
 
-	$stmt->bindParam(':name', $_POST['name']);
+	$query = "INSERT INTO user (name) VALUES (?)";
+
+	$stmt = $db->prepare($query);
+
+	$stmt->bind_param('s', $_POST['name']);
 
 	if ($stmt->execute()) :
+		$name = $_POST['name'];
 		$message = 'Successfully created new user';
 		$_SESSION['name'] = $_POST['name'];
+		$query = "SELECT uid FROM user WHERE name = '$name'";
 
-		header("Location: /home.php");
+		$res = mysqli_query($db, $query);
+		$res = mysqli_fetch_assoc($res);
+		$_SESSION['uid'] = (int) $res['uid'];
+
+		header("Location: /chat.html");
 	else :
 		$message = 'Sorry there must have been an issue creating your account';
 	endif;
@@ -40,8 +47,8 @@ endif;
 <html>
 
 <head>
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
 </head>
@@ -59,12 +66,12 @@ endif;
 	<div class="row">
 		<div class="col-md-2 col-md-offset-5">
 
-			
+
 			<form action="index.php" method="POST">
 
 				<input type="text" placeholder="Enter your name" name="name">
-			
-				
+				<br>
+				<br>
 				<input class="btn btn-primary" type="submit" value="Start">
 
 			</form>
